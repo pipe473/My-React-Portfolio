b7cdcafbe206ff0ca2b1aa359d84cb08d9ad3fe1
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-23 11:54:49 +0200

    comienzo el diseño de mi pagina de inicio

--- src/Pages/Conocimientos/index.jsx
+++ src/Pages/Conocimientos/index.jsx
@@ -7,17 +7,27 @@ function Conocimientos(props) {
 
         <div className="programas">
 
-            <a href='#' target="React.JS"><i class="fab fa-react"></i></a>
-            <a href='#' target="ANGULAR2"></a><i class="fab fa-angular"></i>
-            <a href='#' target="HTML5"></a><i class="fab fa-html5"></i>
-            <a href='#' target="CSS3"></a><i class="fab fa-css3-alt"></i>
-            <a href='#' target="JS"></a><i class="fab fa-js-square"></i>
+            <div className="row1">
 
+                <a href='#' target="React.JS"><i class="fab fa-react"></i></a>
+                <a href='#' target="ANGULAR2"></a><i class="fab fa-angular"></i>
+                <a href='#' target="HTML5"></a><i class="fab fa-html5"></i>
+                <a href='#' target="CSS3"></a><i class="fab fa-css3-alt"></i>
+                <a href='#' target="JS"></a><i class="fab fa-js-square"></i>
+            </div>
+
+            <div className="row2">
+                <a href='#' target="SASS"></a><i class="fab fa-sass"></i>
+                <a href='#' target="NODE.JS"></a><i class="fab fa-node"></i>
+                <a href='#' target="BOOTSTRAP"></a><i class="fab fa-bootstrap"></i>
+                <a href='#' target="DOCKER"></a><i class="fab fa-docker"></i>
+                <a href='#' target="YARN"></a><i class="fab fa-yarn"></i>
+            </div>
 
         </div>
     );    
 }
 
-export default Conocimientos
+export default Conocimientos;
 
 
