05ce914fb698c955221fb4f040e6f1967a90d71b
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-23 14:08:54 +0200

    casi esta la pagina de inicio falta terminar el titulo

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Menu.scss';
 
 function Menu() {
 
     return (
         <div className="menu">
 
-            <NavLink activeClassName="is-active" to="/">Inicio</NavLink>
-            <NavLink activeClassName="is-active" to="/Sobre-mi">Sobre mi</NavLink>
-            <NavLink activeClassName="is-active" to="/Conocimientos">Conocimientos</NavLink>
-            <NavLink activeClassName="is-active" to="/Contacto">Contacto</NavLink>
+            
+            <NavLink  className="menu-item" activeClassName="is-active" to="/">Inicio</NavLink>
+            <NavLink  className="menu-item" activeClassName="is-active" to="/Sobre-mi">Sobre_Mi</NavLink>
+            <NavLink  className="menu-item" activeClassName="is-active" to="/Conocimientos">Conocimientos</NavLink>
+            <NavLink  className="menu-item" activeClassName="is-active" to="/Contacto">Cont@cto</NavLink>
 
 
         </div>
