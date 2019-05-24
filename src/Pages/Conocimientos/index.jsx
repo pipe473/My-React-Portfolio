df23bf1d11300f9356525679ee3e3e6488cc7651
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-24 14:10:14 +0200

    diseño la seccion de conocimientos

--- src/Pages/Conocimientos/index.jsx
+++ src/Pages/Conocimientos/index.jsx
@@ -7,23 +7,25 @@ function Conocimientos(props) {
 
         <div className="programas">
 
-            <div className="row1">
+            <h1>Conocimientos</h1>
 
-                <a href='#' target="React.JS"><i class="fab fa-react"></i></a>
-                <a href='#' target="ANGULAR2"></a><i class="fab fa-angular"></i>
-                <a href='#' target="HTML5"></a><i class="fab fa-html5"></i>
-                <a href='#' target="CSS3"></a><i class="fab fa-css3-alt"></i>
-                <a href='#' target="JS"></a><i class="fab fa-js-square"></i>
+            <div className="row1">
+                <a className="item" href='#' target="React.JS"></a><i className="fab fa-react"></i>
+                <a className="item" href='#' target="ANGULAR2"></a><i className="fab fa-angular"></i>
+                <a className="item" href='#' target="HTML5"></a><i className="fab fa-html5"></i>
+                <a className="item" href='#' target="CSS3"></a><i className="fab fa-css3-alt"></i>
+                <a className="item" href='#' target="JS"></a><i className="fab fa-js-square"></i>
             </div>
 
             <div className="row2">
-                <a href='#' target="SASS"></a><i class="fab fa-sass"></i>
-                <a href='#' target="NODE.JS"></a><i class="fab fa-node"></i>
-                <a href='#' target="BOOTSTRAP"></a><i class="fab fa-bootstrap"></i>
-                <a href='#' target="DOCKER"></a><i class="fab fa-docker"></i>
-                <a href='#' target="YARN"></a><i class="fab fa-yarn"></i>
+                <a className="item" href='#' target="SASS"></a><i class="fab fa-sass"></i>
+                <a className="item" href='#' target="NODE.JS"></a><i class="fab fa-node"></i>
+                <a className="item" href='#' target="BOOTSTRAP"></a><i class="fab fa-bootstrap"></i>
+                <a className="item" href='#' target="DOCKER"></a><i class="fab fa-docker"></i>
+                <a className="item" href='#' target="YARN"></a><i class="fab fa-yarn"></i>
             </div>
 
+           
         </div>
     );    
 }
