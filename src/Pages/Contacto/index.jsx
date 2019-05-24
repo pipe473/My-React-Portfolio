1ada5d9863c79a6f2e33d3aceebe732216d8b390
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-24 18:31:35 +0200

    pagina de inicio responsive

--- src/Pages/Contacto/index.jsx
+++ src/Pages/Contacto/index.jsx
@@ -5,7 +5,7 @@ import './Contacto.scss';
 function Contacto(props) {
     return(
         <div className="contacto">
-            <h1>HOLA HP</h1>
+            <h1>HOLA  HP</h1>
 
         </div>
 
