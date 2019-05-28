06b92f786c327aeb46ae36803a0963d1846b18c1
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-28 10:24:02 +0200

    añado animaciones de prueba a mi nombre, con react

--- src/Pages/Inicio/index.jsx
+++ src/Pages/Inicio/index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './Inicio.scss';
 import Footer from '../../Components/Footer/index';
 
+import ScrollAnimation from 'react-animate-on-scroll';
+
 
 function Inicio(props) {
   return (
     <div className="Inicio">
       <div className="Informacion">
-        <p className="nombre">Felipe Bedoya</p>
+        <ScrollAnimation animateIn="flip" duration="5">
+          <p className="nombre">Felipe Bedoya</p>
+        </ScrollAnimation>
       </div>
       <Footer />
     </div>
