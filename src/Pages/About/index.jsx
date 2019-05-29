ad3c8b7f4951c0013d7d59b0facaf6366a5e8835
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-29 10:33:19 +0200

    añadiendo efectos a la seccion sobre mi

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './About.scss';
 
+import ScrollAnimation from 'react-animate-on-scroll';
+
 function About(props) {
   return (
     <div className="About-me">
       <h1 className="titulo">Sobre mi</h1>
-
+      <ScrollAnimation animateIn="fadeInRightBig" duration="3">
       <p className="parrafo">
         Persona apasionada por la creatividad y el diseño, con interés por las
         nuevas tecnologias y el aprendizaje constante. Con experiencia en el
         diseño industrial y la delineación, habituado al trabajo en equipo y con
         habilidades resolutivas...
       </p>
+      </ScrollAnimation>
     </div>
   );
 }
