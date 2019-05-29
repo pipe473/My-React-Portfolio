76e578b58c93addc098261716b961c33dd1a1e32
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-05-29 11:15:22 +0200

    seccion sobre mi terminada con animaiones incluidas

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -7,12 +7,24 @@ function About(props) {
   return (
     <div className="About-me">
       <h1 className="titulo">Sobre mi</h1>
-      <ScrollAnimation animateIn="fadeInRightBig" duration="3">
-      <p className="parrafo">
-        Persona apasionada por la creatividad y el diseño, con interés por las
-        nuevas tecnologias y el aprendizaje constante. Con experiencia en el
-        diseño industrial y la delineación, habituado al trabajo en equipo y con
-        habilidades resolutivas...
+      <ScrollAnimation animateIn="fadeInRightBig" duration="2">
+      <p className="parrafo1">
+        Apasionado por la creatividad y el diseño,
+        </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInRightBig" duration="3">
+        <p className="parrafo2">
+        con interés por las nuevas tecnologias y el aprendizaje constante. 
+        </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInRightBig" duration="4">
+        <p className="parrafo3">
+        Con experiencia en el diseño industrial y la delineación,
+        </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInRightBig" duration="5">
+        <p className="parrafo4">
+        habituado al trabajo en equipo y con habilidades resolutivas...
       </p>
       </ScrollAnimation>
     </div>
