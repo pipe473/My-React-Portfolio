1e7477d4519ce113c0f47396d86ca36e16bd551b
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-06-06 20:07:10 +0200

    seccion de conocimientos terminada

--- src/Pages/Conocimientos/index.jsx
+++ src/Pages/Conocimientos/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Conocimientos.scss';
 
+import ScrollAnimation from 'react-animate-on-scroll';
+
 function Conocimientos(props) {
 
     return(
@@ -8,22 +10,25 @@ function Conocimientos(props) {
         <div className="programas">
 
             <h1>Conocimientos</h1>
-
+            <ScrollAnimation animateIn="bounceInDown" duration="2">
                 <div className="column1">
                     <a className="item" href='#' target="SASS"></a><i class="fab fa-sass"></i>
                     <a className="item" href='#' target="NODE.JS"></a><i class="fab fa-node"></i>
                     <a className="item" href='#' target="BOOTSTRAP"></a><i class="fab fa-bootstrap"></i>
                     <a className="item" href='#' target="HTML5"></a><i class="fab fa-html5"></i>
-                    <a className="item" href='#' target="CSS3"></a><i class="fab fa-css3-alt">...</i>
+                    <a className="item" href='#' target="CSS3"></a><i class="fab fa-css3-alt"></i>
                 </div>
+                </ScrollAnimation>
 
+                <ScrollAnimation animateIn="bounceInUp" duration="3">
                 <div className="column2">
                     <a className="item" href='#' target="React.JS"></a><i class="fab fa-react"></i>
                     <a className="item" href='#' target="YARN"></a><i class="fab fa-yarn"></i>
                     <a className="item" href='#' target="ANGULAR2"></a><i class="fab fa-angular"></i>               
                     <a className="item" href='#' target="JS"></a><i class="fab fa-js-square"></i>
-                    <a className="item" href='#' target="DOCKER"></a><i class="fab fa-docker"></i> 
-                </div>            
+                    <a className="item" href='#' target="DOCKER"></a><i class="fab fa-docker">...</i> 
+                </div>    
+                </ScrollAnimation>        
             </div>
     );    
 }
