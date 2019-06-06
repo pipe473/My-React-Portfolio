8fc4027e2c7cd30ed69946cc9d155232cefe2adf
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-06-06 23:57:01 +0200

    seccion about me responsive terminada

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -7,22 +7,21 @@ function About(props) {
   return (
     <div className="About-me">
       <h1 className="titulo">Sobre mi</h1>
-      <ScrollAnimation animateIn="fadeInRightBig" duration="2">
+      <ScrollAnimation animateIn="fadeInUp" duration="2">
       <p className="parrafo1">
         Apasionado por la creatividad y el diseño,
         </p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInRightBig" duration="3">
+                
         <p className="parrafo2">
         con interés por las nuevas tecnologias y el aprendizaje constante. 
         </p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInRightBig" duration="4">
+        <ScrollAnimation animateIn="fadeInRightBig" duration="6">
         <p className="parrafo3">
         Con experiencia en el diseño industrial y la delineación,
         </p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInRightBig" duration="5">
+        <ScrollAnimation animateIn="fadeInRightBig" duration="8">
         <p className="parrafo4">
         habituado al trabajo en equipo y con habilidades resolutivas...
       </p>
