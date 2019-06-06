c2ae81f34e65a5c63aac3e0bf3cbb057e72ace1e
Felipe Bedoya <felipe.pipe473@gmail.com>
2019-06-06 14:40:34 +0200

    subiendo docs produccion

---
+++ docs/precache-manifest.b4ab84a1355b2bc54141a1ab395bbacd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e88a51b1d17c90bae207cb38f38edfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cba35da3d80bf2cb232f",
+    "url": "/static/css/main.12470e92.chunk.css"
+  },
+  {
+    "revision": "37a4239f93d268cb251c",
+    "url": "/static/js/2.ea8bdcb7.chunk.js"
+  },
+  {
+    "revision": "cba35da3d80bf2cb232f",
+    "url": "/static/js/main.d6590314.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
